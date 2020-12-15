84fe92fb7753e96805fb918edc6814ba5cd14d90
Crypsack <63713301+Zizi32@users.noreply.github.com>
2020-12-15 22:16:42 +0530

    Add files via upload

---
+++ Template/_banner-area.php
@@ -0,0 +1,15 @@
+<!-- Owl-carousel -->
+<section id="banner-area">
+    <div class="owl-carousel owl-theme">
+        <div class="item">
+            <img src="./assets/Banner1.png" alt="Banner2">  
+        </div>
+        <div class="item">
+            <img src="./assets/Banner2.png" alt="Banner2">
+        </div>
+        <div class="item">
+            <img src="./assets/Banner3.jpg" alt="Banner3">
+        </div>
+    </div>
+</section>
+<!-- !Owl-carousel -->
---
+++ Template/_blogs.php
@@ -0,0 +1,36 @@
+
+<!-- Blogs -->
+<section id="blogs">
+    <div class="container py-4">
+        <h4 class="font-rubik font-size-20">STAY UPDATED!</h4>
+        <hr>
+
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Covid Precaution methods</h5>
+                    <img src="./assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis non iste sequi cupiditate tempora iure. Velit accusamus saepe harum sed.</p>
+                    <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public" class="color-second text-left">Check out</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Latest covid news</h5>
+                    <img src="./assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis non iste sequi cupiditate tempora iure. Velit accusamus saepe harum sed.</p>
+                    <a href="https://www.timesnownews.com/international/article/covid-19-could-last-on-surfaces-up-to-28-days-latest-findings-a-cause-for-concern-but-no-reason-to-panic/667625" class="color-second text-left">Check out</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Latest covid news</h5>
+                    <img src="./assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis non iste sequi cupiditate tempora iure. Velit accusamus saepe harum sed.</p>
+                    <a href="https://indianexpress.com/article/explained/india-coronavirus-active-cases-deaths-recoveries-explained-october-15-6737721/" class="color-second text-left">Check out</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- !Blogs -->
